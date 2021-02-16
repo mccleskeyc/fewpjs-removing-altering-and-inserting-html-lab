9808afbc64e3b22fa112862c37da4715005a854e
mccleskeyc <mccleskeyc@gmail.com>
2021-02-16 11:36:20 -0500

    commit tree 642d6d9cf678e4b4666e71562900ba9a690aa944

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+/* removes main */
+main.remove()
+
+/* creates cons variable newHeader set to a newly created h1 element*/ 
+const newHeader = document.createElement("h1");
+
+/* sets newHeader id to victory */
+newHeader.id = "victory"
+
+/* is the champion message */
+newHeader.innerHTML = '${name} is the champion!'
